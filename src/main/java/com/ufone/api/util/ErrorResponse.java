613d64f26eddb3076f5cfbcca690a073d74fff4a
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-07 20:32:53 +0500

    Error classes

--- src/main/java/com/ufone/api/util/ErrorResponse.java
+++ src/main/java/com/ufone/api/util/ErrorResponse.java
@@ -1,3 +1,4 @@
+// Copyright 2019 Shehriyar Qureshi
 package com.ufone.api.util;
 
 import com.google.gson.annotations.SerializedName;
--- src/main/java/com/ufone/api/util/InvalidRequest.java
+++ src/main/java/com/ufone/api/util/InvalidRequest.java
@@ -1,3 +1,4 @@
+// Copyright 2019 Shehriyar Qureshi
 package com.ufone.api.util;
 
 public class InvalidRequest {
