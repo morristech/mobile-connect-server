d1bc4c0b569aa6c93d00c2c8461355b5f21496aa
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-07 11:38:02 +0500

    Change name of war file when moving to tomcat

--- send_to_tom.sh
+++ send_to_tom.sh
@@ -1,6 +1,6 @@
 # Maven Build
 mvn clean verify
 # Copy to tommcat
-sudo cp target/oidc-server.war /usr/share/tomcat8/webapps/
+sudo cp target/oidc-server.war /usr/share/tomcat8/webapps/oidc.war
 # Restart tomcat
 sudo systemctl restart tomcat8
