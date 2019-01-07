3d253e9002c8688ec4b30ae3ddb6864f780ab023
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-07 11:40:11 +0500

    Add error to util package (will move them soon)

---
+++ src/main/java/com/ufone/api/util/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.ufone.api.util;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ErrorResponse {
+        @SerializedName("error")
+        private String error;
+        @SerializedName("error_description")
+        private String errorDescription;
+
+
+        public ErrorResponse() {}
+
+        public ErrorResponse(String error, String errorDescription) {
+                this.error = error;
+                this.errorDescription = errorDescription;
+        }
+
+}
---
+++ src/main/java/com/ufone/api/util/InvalidRequest.java
@@ -0,0 +1,12 @@
+package com.ufone.api.util;
+
+public class InvalidRequest {
+        public String error;
+        public String errorDescription;
+
+
+        public InvalidRequest(String error, String errorDescription) {
+                this.error = error;
+                this.errorDescription = errorDescription;
+        }
+}
