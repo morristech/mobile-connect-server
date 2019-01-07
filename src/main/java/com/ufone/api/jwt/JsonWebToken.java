04328cf482df055e8667a2ab51ba5d6a53aa581a
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-07 20:35:19 +0500

    Just added this file, doesn't do anything

---
+++ src/main/java/com/ufone/api/jwt/JsonWebToken.java
@@ -0,0 +1,24 @@
+package com.ufone.api.util;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+public class JsonWebToken {
+        @SerializedName("iss")
+        private String issuer;
+        @SerializedName("exp")
+        private double expiry;
+        @SerializedName("typ")
+        private String type;
+        @SerializedName("alg")
+        private String algorithm;
+
+        public boolean isValidFormat(String jsonWebToken) {
+                return true;
+        }
+
+        public boolean isNotTampered(String jsonWebToken) {
+                return true;
+        }
+
+}
