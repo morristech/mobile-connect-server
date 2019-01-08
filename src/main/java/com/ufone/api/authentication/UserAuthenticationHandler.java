54021f1092265cf2d4efd3745a77a72d30c66e59
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-08 11:04:29 +0500

    Add empty class which handles authentication

---
+++ src/main/java/com/ufone/api/authentication/UserAuthenticationHandler.java
@@ -0,0 +1,21 @@
+package com.ufone.api.authentication;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
+/*
+ * This class is responsible for redirecting the user to wait screen and initiating authentication
+ */
+public class UserAuthenticationHandler {
+        private final String waitScreenURI = "https://localhost:8080/mobile_connect/wait";
+
+        public Response redirectToWaitScreen() {
+                return Response.status(Response.Status.MOVED_PERMANENTLY).header("Location", waitScreenURI).build();
+        }
+}
