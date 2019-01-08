d0e48a7471db63313775a99c318a4aceeca5f395
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-08 11:03:43 +0500

    Formatting and add comment

--- src/main/java/com/ufone/api/util/ErrorResponse.java
+++ src/main/java/com/ufone/api/util/ErrorResponse.java
@@ -6,11 +6,12 @@ import com.google.gson.annotations.SerializedName;
 public class ErrorResponse {
         @SerializedName("error")
         private String error;
+        // this is optional in OIDC but required for Mobile Connect
         @SerializedName("error_description")
         private String errorDescription;
 
-
-        public ErrorResponse() {}
+        public ErrorResponse() {
+        }
 
         public ErrorResponse(String error, String errorDescription) {
                 this.error = error;
