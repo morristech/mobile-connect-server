483a7dc1ab407a0362e2be3cd0a20ede34a918a8
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-09 12:31:51 +0500

    Add some docs

--- src/main/java/com/ufone/api/util/PCRGeneration.java
+++ src/main/java/com/ufone/api/util/PCRGeneration.java
@@ -4,14 +4,23 @@ package com.ufone.api.util;
 import java.util.UUID;
 
 /*
- * This class is responsible for generating a unique PCR.
- * It DOES NOT handle pcr and msisdn mapping. It's a utility function which
- * only serves a small purpose. That should be handled by another class.
+ * This class is responsible for generating a unique PCR. It DOES NOT handle pcr
+ * and msisdn mapping. It's a utility function which only serves a small
+ * purpose. That should be handled by another class.
  *
  * The pcr is in the UUID4 format.
+ *
+ * @author Shehriyar Qureshi
  */
 public class PCRGeneration {
 
+        /*
+         * Generate UUID
+         *
+         * @author Shehriyar Qureshi
+         *
+         * @return pcr generated pcr to be used to map to a MSISDN.
+         */
         public String generatePCR() {
                 UUID uuid = UUID.randomUUID();
                 String pcr = uuid.toString();
