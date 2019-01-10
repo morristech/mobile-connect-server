e87edcdd9af88808c01fc797d1286632cef86a65
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-10 10:50:11 +0500

    Update name metadata in pom.xml & script

--- send_to_tom.sh
+++ send_to_tom.sh
@@ -1,6 +1,6 @@
 # Maven Build
 mvn clean verify
 # Copy to tommcat
-sudo cp target/oidc-server.war /usr/share/tomcat8/webapps/oidc.war
+sudo cp target/mobile-connect-server.war /usr/share/tomcat8/webapps/oidc.war
 # Restart tomcat
 sudo systemctl restart tomcat8
