6f8919eaf1fb12d8491f8c74ef3b8b98aec038ae
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-10 10:44:13 +0500

    Make openid-configuration values static
    I'm still working on completely understanding static variables.
    Making them static should reduce memory usage.

--- src/main/java/com/ufone/api/discovery/DiscoveryConfiguration.java
+++ src/main/java/com/ufone/api/discovery/DiscoveryConfiguration.java
@@ -5,47 +5,41 @@ import com.google.gson.annotations.SerializedName;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class DiscoveryConfiguration {
-        private static volatile DiscoveryConfiguration configInstance = null;
+import java.lang.reflect.Modifier;
 
+public class DiscoveryConfiguration {
         // NOTE: Not reading from file or something as that's gonna slow down
         // performance plus these fields won't be changed often so hardcoding them here
         // shouldn't be a problem and they will be compiled to bytecode so they can't be
         // changed by someone which might have malicious intent.
-        @SerializedName("issuer") private final String issuer = "https://api.ufone.com";
+        @SerializedName("issuer") private static final String issuer = "https://api.ufone.com";
         @SerializedName("authorization_endpoint")
-        private final String authorizationEndpoint = "https://api.ufone.com/oidc/authorize";
+        private static final String authorizationEndpoint = "https://api.ufone.com/oidc/authorize";
         @SerializedName("token_endpoint")
-        private final String tokenEndpoint = "https://api.ufone.com/token";
+        private static final String tokenEndpoint = "https://api.ufone.com/token";
         @SerializedName("userinfo_endpoint")
-        private final String userinfoEndpoint = "https://api.ufone.com/oidc/userinfo";
+        private static final String userinfoEndpoint = "https://api.ufone.com/oidc/userinfo";
         @SerializedName("revocation_endpoint")
-        private final String revocationEndpoint = "https://api.ufone.com/oidc/revoke";
+        private static final String revocationEndpoint = "https://api.ufone.com/oidc/revoke";
         @SerializedName("jwks_uri")
-        private final String jwksURI = "https://api.ufone.com/oidc/certs";
+        private static final String jwksURI = "https://api.ufone.com/oidc/certs";
         @SerializedName("response_types_supported")
-        private final String[] responseTypesSupported = {"code"};
-        @SerializedName("scopes_supported") private final String[] scopesSupported = {"openid"};
+        private static final String[] responseTypesSupported = {"code"};
+        @SerializedName("scopes_supported")
+        private static final String[] scopesSupported = {"openid"};
         @SerializedName("subject_types_supported")
-        private final String[] subject_types_supported = {"public"};
+        private static final String[] subject_types_supported = {"public"};
         @SerializedName("id_token_signing_alg_values_supported")
-        private final String[] tokenSigningAlgorithm = {"RS256"};
-        @SerializedName("acr_values_supported") private final String[] acrValuesSupported = {"2"};
-
-        private DiscoveryConfiguration() {}
+        private static final String[] tokenSigningAlgorithm = {"RS256"};
+        @SerializedName("acr_values_supported")
+        private static final String[] acrValuesSupported = {"2"};
 
         public static String getResponseAsString() {
-                // Singleton class, don't need to create an object on every request
-                if (configInstance == null) {
-                        // pretty output, only google does this, other's dunno
-                        Gson jsonResponse = new GsonBuilder().setPrettyPrinting().create();
-                        configInstance = new DiscoveryConfiguration();
-                        String responseBody = jsonResponse.toJson(configInstance);
-                        return responseBody;
-                } else {
-                        Gson jsonResponse = new GsonBuilder().setPrettyPrinting().create();
-                        String responseBody = jsonResponse.toJson(configInstance);
-                        return responseBody;
-                }
+                Gson jsonResponse = new GsonBuilder()
+                                        .excludeFieldsWithModifiers(Modifier.TRANSIENT)
+                                        .setPrettyPrinting()
+                                        .create();
+                String responseBody = jsonResponse.toJson(new DiscoveryConfiguration());
+                return responseBody;
         }
 }
