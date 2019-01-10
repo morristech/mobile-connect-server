6c1a4e25f5a0a44de5085aa4e7210d232838e6e8
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-10 10:40:23 +0500

    Add serialVersionUID for exceptions

--- src/main/java/com/ufone/api/exceptions/MissingClientIDException.java
+++ src/main/java/com/ufone/api/exceptions/MissingClientIDException.java
@@ -1,6 +1,7 @@
 package com.ufone.api.exceptions;
 
 public class MissingClientIDException extends Exception {
+        private static final long serialVersionUID = 291092L;
         public MissingClientIDException() {
                 super("MANDATORY parameter client_id is missing");
         }
--- src/main/java/com/ufone/api/exceptions/MissingScopeException.java
+++ src/main/java/com/ufone/api/exceptions/MissingScopeException.java
@@ -1,6 +1,7 @@
 package com.ufone.api.exceptions;
 
 public class MissingScopeException extends Exception {
+        private static final long serialVersionUID = 91859124L;
         public MissingScopeException() {
                 super("MANDATORY parameter state is missing");
         }
